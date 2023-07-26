a74a98f1805182e408679a23088aa39b72dd59e3
Venkata Krishna Reddy <venkatakrishna9642@gmail.com>
2023-07-26 16:59:53 +0530

    Added the changes

---
+++ src/Pages/AboutUsPage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './AboutUsPage.css'; // Import your custom CSS file here
+
+const AboutUsPage = () => {
+
+    return(
+
+        <div class="about-container">
+           <h1> In progress</h1>
+
+           </div>
+    
+    )
+
+};
+
+export default AboutUsPage;

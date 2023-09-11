4a601de158c867ebb8340a4e47aa1854c4315728
Narasimha969 <narasimhareddy.koppula@gmail.com>
2023-09-11 11:28:13 +0530

    footer logo changes

--- src/Pages/AboutUsPage.js
+++ src/Pages/AboutUsPage.js
@@ -1,8 +1,21 @@
-import React from 'react';
+import React,{useState} from 'react';
 import Footer from '../Components/Footer';
-import './AboutUsPage.css'; // Import your custom CSS file here
+import './AboutUsPage.css';
+import ConnectModal from '../Components/ConnectModal';
 
 const AboutUsPage = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const openPopup = () => {
+    setIsOpen(true);
+    document.body.style.overflow = 'hidden';
+  };
+
+  const closePopup = () => {
+
+    setIsOpen(false);
+    document.body.style.overflow = 'auto';
+  };
 
     return(
 
@@ -22,74 +35,95 @@ const AboutUsPage = () => {
       <div className="col-md-6">
         <div className="row g-0  overflow-hidden flex-md-row mb-4 h-md-250 position-relative">
           <div className="col p-4 d-flex flex-column position-static">
-              <div className="d-inline-block mb-2 text-primary-emphasis">
-                <span className="text-success title">Our Story</span>
+          <div className="mb-0">
+          Welcome to We Fund Your Future, your trusted partner in achieving your educational dreams. We understand that education is the key to a brighter future, and we are dedicated to helping you access the financial support you need to pursue your educational goals            </div>
+              <div className="d-inline-block mt-3 text-primary-emphasis">
+                <span className="text-success title">Who We Are</span>
               </div>
-            <div className="mb-0">
-            At We Fund Your Future, we have a proven track record of providing educational loans to support students in pursuing their academic dreams
-            </div>
+           
             <span className="float">
-              <p className="card-text mb-auto">Our experience in offering educational loans sets us apart as a trusted financial partner for students and their families.
-              We offer affordable financing with competitive interest rates. Our streamlined online process makes applying quick and hassle-free.
-              We operate with transparency and fairness.Our friendly team is ready to assist you at every step.
+              <p className="card-text mb-auto">At We Fund Your Future, we are a team of dedicated professionals who act as Direct Selling Agents (DSAs) for educational loans. We have a passion for empowering individuals to reach their full potential through education, and we believe that financial barriers should never stand in the way of learning.
               </p>
             </span>
+
+
+            <div className="d-inline-block mt-3 text-primary-emphasis">
+                <span className="text-success title">Our Mission:</span>
+              </div>
+           
+            <span className="float">
+              <p className="card-text mb-auto">Our mission is simple yet powerful: to make quality education accessible to everyone. We are committed to assisting students, parents, and institutions in securing the necessary funding to pursue higher education. Through our expertise and partnerships with leading financial institutions, we aim to simplify the loan application process and provide tailored solutions that fit your unique needs.
+              </p>
+            </span>
+
+
+            <div className="d-inline-block mt-3 text-primary-emphasis">
+               <span className="text-success title">What We Offer:</span>
+              </div>
+           
+            <span className="float">
+              <p className="card-text mb-auto">
+                <ol>
+                  <li  > <span style={{fontFamily: 'Gotham Medium'}}>Expert Guidance </span>: Our team of experienced financial advisors will guide you through the loan application process, ensuring you have all the information you need to make informed decisions</li>
+                  <li><span style={{fontFamily: 'Gotham Medium'}}>Customer-Centric Approach</span>: We understand that every student's financial situation is unique. We work closely with you to tailor loan options that align with your specific requirements.</li>
+                  <li><span style={{fontFamily: 'Gotham Medium'}}>Innovation</span>: We have established strong relationships with a network of reputable financial institutions, allowing us to connect you with lenders offering competitive interest rates and flexible repayment terms</li>
+                  <li><span style={{fontFamily: 'Gotham Medium'}}>Partnerships</span>: We uphold the highest standards of transparency and ethics in all our interactions. You can trust us to provide honest advice and ensure that your interests are always at the forefront.</li>
+                </ol>
+              </p>
+            </span>
+
+
+
+
+
+
+
+
+
           </div>
           
         </div>
-        <div className="col misson-container d-flex flex-column position-static">
-
-              <div className="d-inline-block mb-2 text-primary-emphasis">
-                <span className="text-success title">Our Mission</span>
-              </div>
-
-            <p className="card-text ">When you choose We Fund Your Future for your educational loan, you benefit from our extensive experience and dedication to helping students fulfill their educational aspirations. Trust us to provide the financial support you need to embark on a successful educational journey.</p>
-            <div className="percentage">
-              <div>
-                <h1>20 +</h1>
-                <div>
-                  <p>Countries</p>
-                </div>
-              </div>
-              <div>
-                <h1>10 +</h1>
-                <div>
-                  <p>Lenders</p>
-                </div>
-              </div>
-              <div>
-                <h1>5k +</h1>
-                <div>
-                  <p>Loans Facilitated</p>
-                </div>
-              </div>
-            </div>
-          </div>
+       
       </div>
       <div className="col-md-6">
         <div className="row g-0  overflow-hidden flex-md-row mb-4  h-md-250 position-relative">
          
           <div className="col p-4 d-flex flex-column position-static">
-          <h4>
-              <strong className="d-inline-block mb-2 text-primary-emphasis">
-                <b><span className="title normal">Here's why our experience makes us the ideal choice for your educational loan needs:</span></b>
-              </strong>
-            </h4>
-            <h3 className="mb-0 title"><span className="number text-success">01. </span>Specialized Focus</h3>
-            <div className="mb-2 text-body-secondary small margin sub-text">We understand the unique financial challenges students face when it comes to funding their education. Our specialized focus on educational loans allows us to cater to the specific needs of students, offering tailored solutions to make education more accessible.</div>
-            <h3 className="mb-0 title"><span className="number text-success">02. </span>Extensive Network</h3>
-            <div className="mb-2 text-body-secondary small margin">Over the years, we have built strong relationships with educational institutions and organizations, enabling us to collaborate closely with them to streamline the loan application and disbursement process. This network ensures a seamless experience for borrowers.</div>
-            <h3 className="mb-0 title"><span className="number text-success">03. </span>Knowledgeable Advisors</h3>
-            <div className="mb-2 text-body-secondary small margin"> Our team of loan advisors possesses in-depth knowledge of the education loan landscape. They are well-versed in the intricacies of student financing and can guide borrowers through the application process, repayment options, and other related financial matters</div>
-            <h3 className="mb-0 title"><span className="number text-success">04. </span>Customer Success Stories</h3>
-            <div className="mb-2 text-body-secondary small margin">Our satisfied customers are a testament to our expertise in providing educational loans. Numerous students have achieved their educational goals with our support, benefiting from our competitive interest rates, flexible repayment plans, and efficient service</div>
-            <h3 className="mb-0 title"><span className="number text-success">05. </span>Continuous Improvement</h3>
-            <div className="mb-2 text-body-secondary small margin">We continually strive to enhance our educational loan offerings based on feedback and market trends. By staying updated on evolving educational financing needs, we ensure that our loan products align with the changing demands of students and their families.</div>
-            <h3 className="mb-0 title"><span className="number text-success">06. </span>Financial Education Initiatives</h3>
-            <div className="mb-2 text-body-secondary small margin">We go beyond just providing loans by promoting financial literacy among borrowers. Through various initiatives, we empower students to make informed financial decisions, manage their finances responsibly, and build a strong foundation for their future.</div>
-            <h3 className="mb-0 title"><span className="number text-success">07. </span>Longevity and Stability</h3>
-            <div className="mb-2 text-body-secondary small margin">With a long-standing presence in the financial industry, We Fund Your Future has established a reputation for stability and reliability. Our commitment to ethical practices, transparency, and fair dealings has earned us the trust of our customers.</div>
+      
+              
+            <span className="text-success title mb-2">Why Choose Us: </span>
+        
+         
+            <h3 className="mb-0 title"><span className="number text-success">01. </span>Experience : </h3>
+            <div className="mb-2 text-body-secondary small margin sub-text">With years of experience in the education financing industry, we have the knowledge and expertise to navigate the complexities of loan applications.</div>
+            <h3 className="mb-0 title"><span className="number text-success">02. </span>Customer-Centric Approach : </h3>
+            <div className="mb-2 text-body-secondary small margin sub-text">Your satisfaction is our priority. We are committed to providing exceptional customer service and support throughout your educational journey</div>
+            <h3 className="mb-0 title"><span className="number text-success">03. </span>Innovation :</h3>
+            <div className="mb-2 text-body-secondary small margin sub-text"> We stay updated with the latest trends and technologies in education financing to offer you the most efficient and convenient solutions.</div>
+            <h3 className="mb-0 title"><span className="number text-success">04. </span>Partnerships : </h3>
+            <div className="mb-2 text-body-secondary small margin sub-text">Our strong partnerships with leading banks and financial institutions mean you have access to a wide range of loan options</div>
+          
+          
+            <span className="text-success title"  style={{marginTop: '15px'}}>Contact Us : </span>
+            <span className="float">
+              <p className="card-text mb-auto">Ready to take the next step in your educational journey? Contact us today to explore your financing options and kickstart your future.
+              </p>
+            </span>
+
+
+            <button className="HomeBanner_bannerBtnDesktop__K9OFK connectWithUsBtn">
+          <p className="HomeBanner_btnText__yMOPW" onClick={openPopup}>Connect with us</p>
+          <ConnectModal isOpen={isOpen} onRequestClose={closePopup} />
+        </button>
+
+
+
+          
+          
           </div>
+          
+
+          
         </div>
       </div>
     </div>
